1e3034333927c3695402904a17e83aad069bab01
osmanarslan07 <osmanarslan07@yahoo.com>
2023-01-04 23:02:47 +0300

    day 41 : Exceptions

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,8 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
 
     public static void main(String[] args) {
@@ -62,9 +65,27 @@ public class TryCatchBlocks {
 
         System.out.println("Test3 completed");
 
+        System.out.println("----------------------------------");
+
+        System.out.println("Hello");
+
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+              e.printStackTrace();
+        }
 
 
+        System.out.println("Cydeo");
 
+        System.out.println("------------------------------------------");
+
+
+        try {
+            FileInputStream file = new FileInputStream("File path");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
 
 
 

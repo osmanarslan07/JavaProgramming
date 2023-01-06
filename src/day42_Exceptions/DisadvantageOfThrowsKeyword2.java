4948d5bebdddc60b8c6220031c6e775e7cd54637
osmanarslan07 <osmanarslan07@yahoo.com>
2023-01-06 23:55:45 +0300

    day 42 : Exceptions

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -7,31 +7,45 @@ import static day34_GarbageCollection_AccessModifiers.AccessModifiers.method1;
 
 public class DisadvantageOfThrowsKeyword2 {
 
-    public static void method(){
+    public static void main(String[] args) throws InterruptedException,NullPointerException{
+        method1();
+        method2();
+        method3();
+
+        String str = null;
+        try {
+            System.out.println(str.charAt(0));
+        } catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void method() throws InterruptedException{
         try {
             method1();
-        } catch (FileNotFoundException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    public static void method1() throws FileNotFoundException {
-        new FileInputStream("");
-
+    public static void method1() throws InterruptedException {
+       // new FileInputStream("");
+         Thread.sleep(3000);
 
     }
 
-    public static void method2() throws FileNotFoundException{
+    public static void method2() throws InterruptedException{
         method1();
 
     }
 
-public static void method3() throws FileNotFoundException, InterruptedException{
+public static void method3() throws InterruptedException{
         method2();
         Thread.sleep(1000);
 }
 
-public static void method4()throws FileNotFoundException, InterruptedException{
+public static void method4()throws InterruptedException{
         method3();
 }
 
---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,27 @@
+package day42_Exceptions;
+
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+
+    public static void main(String[] args) {
+
+        throw new NoSuchElementException("There is no such element as break");
+
+        //throw new InterruptedException();
+
+        //  System.out.println("hello world");
+
+        //  throw new Rectangle(5,7);
+
+
+
+
+
+    }
+
+
+
+
+}

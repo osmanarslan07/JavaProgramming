24bce59732b75943a2d91604abfd89f47a6e8d54
osmanarslan07 <osmanarslan07@yahoo.com>
2023-01-08 21:07:59 +0300

    day 43 : Abstractions

--- src/day43_Abstraction/employeeTask/Developer.java
+++ src/day43_Abstraction/employeeTask/Developer.java
@@ -1,4 +1,22 @@
 package day43_Abstraction.employeeTask;
 
-public class Developer {
+public final class Developer extends Employee{
+    public Developer(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
+
+
+    public void work() {
+        System.out.println(getName()+" is developing application");
+    }
+
+
+    public void sleep() {
+        System.out.println(getName()+ " sleeps 7 hours");
+    }
+
+    public void unitTest(){
+        System.out.println(getName()+ " is unit testing");
+    }
+
 }
